85db7ff0cea63345e7e85189e47e8dbe9f5fb642
Sanaa-hub <sanaamemon567@gmail.com>
2023-03-22 01:41:23 +0500

    working on adding eglish only first

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:localization/home.dart';
+import 'package:localization/language.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      translations: Languages(),
+      home: Home(),
+    );
+  }
+}
